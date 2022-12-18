a690ad88b3aeba355cd34b7c70454ad1753a1557
GokceAltunkanat00 <gold.gokce@hotmail.com>
2022-12-18 20:31:39 +0100

    Unit-tests

--- BYT_CLD_Structure/src/Tests/OrderTest.java
+++ BYT_CLD_Structure/src/Tests/OrderTest.java
@@ -1,58 +1,85 @@
 package Tests;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Models.Order;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
     //TODO: Fill those test cases
-
+    static Order order;
     //Create necessary object here but, don't initialize them here!
     @BeforeAll
     static void setUp() {
+        order=new Order(LocalDate.of(2022, 10, 15), Optional.of(LocalDate.of(2022,10,30)),1,"address123");
+
         //Initialize those objects here.
     }
 
     @Test
     void getId() {
+        int id=1;
+        assertEquals(id,order.getId());
     }
 
     @Test
     void getDateCreated() {
+        order.setDateCreated(LocalDate.of(2022, 10, 15));
+        assertEquals(LocalDate.of(2022, 10, 15),order.getDateCreated());
     }
 
     @Test
     void setDateCreated() {
+        order.setDateCreated(LocalDate.of(2022, 10, 12));
+        assertEquals(LocalDate.of(2022, 10, 12),order.getDateCreated());
     }
 
     @Test
     void getDateFinished() {
+        assertEquals(Optional.of(LocalDate.of(2022,10,30)),order.getDateFinished());
     }
 
     @Test
     void setDateFinished() {
+        order.setDateFinished((Optional.of(LocalDate.of(2022,10,10))));
+        assertEquals(Optional.of(LocalDate.of(2022,10,10)),order.getDateFinished());
+
     }
 
     @Test
     void getStatus() {
+        order.setStatus(1);
+        assertEquals("in progress",order.getStatus());
+
     }
 
     @Test
     void setStatus() {
+        order.setStatus(2);
+        assertEquals("finished",order.getStatus());
     }
 
     @Test
     void getDeliveryAddress() {
+        assertEquals("address123",order.getDeliveryAddress());
     }
 
     @Test
     void setDeliveryAddress() {
+        order.setDeliveryAddress("adress333");
+        assertEquals("adress333",order.getDeliveryAddress());
     }
 
     @Test
     void getStatusType() {
+        order.getStatusType(1);
+        assertEquals("in progress",order.getStatusType(1));
+
     }
 }
--- BYT_CLD_Structure/src/Tests/UserTest.java
+++ BYT_CLD_Structure/src/Tests/UserTest.java
@@ -1,6 +1,8 @@
 package Tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,66 +11,91 @@ import Models.User;
 
 class UserTest {
     //TODO: Fill those test cases
-
+    static User user;
     //Create necessary object here but, don't initialize them here!
     @BeforeAll
     static void setUp() {
+        user=new User("gokce","hektor","hek459","gokce@outlook.com",0);
         //Initialize those objects here.
     }
 
     @Test
     void getId() {
+        int id=1;
+        assertEquals(id,user.getId());
     }
 
     @Test
     void getLogin() {
+        user.setLogin("gokce");
+        assertEquals("gokce",user.getLogin());
     }
 
     @Test
     void setLogin() {
+        user.setLogin("gokce1");
+        assertEquals("gokce1",user.getLogin());
     }
 
     @Test
     void getPassword() {
+        user.setPassword("hektor");
+        assertEquals("hektor",user.getPassword());
     }
 
     @Test
     void setPassword() {
+        user.setPassword("gok");
+        assertEquals("gok",user.getPassword());
     }
 
     @Test
     void getNickname() {
+        assertEquals("hek459",user.getNickname());
     }
 
     @Test
     void setNickname() {
+        user.setNickname("gok459");
+        assertEquals("gok459",user.getNickname());
     }
 
     @Test
     void getEmailAddress() {
+        assertEquals("gokce@outlook.com",user.getEmailAddress());
     }
 
     @Test
     void setEmailAddress() {
+        user.setEmailAddress("g@outlook.com");
+        assertEquals("g@outlook.com",user.getEmailAddress());
     }
 
     @Test
     void getProfilePicture() {
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> user.getProfilePicture(), "Exception is expected!");
+
     }
 
     @Test
     void setProfilePicture() {
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> user.setProfilePicture(null), "Exception is expected!");
+
     }
 
     @Test
     void getAccountStatus() {
+        assertEquals("active",user.getAccountStatus());
     }
 
     @Test
     void setAccountStatus() {
+        user.setAccountStatus(1);
+        assertEquals("suspended",user.getAccountStatus());
     }
 
     @Test
     void getStatusType() {
+        assertEquals("active",user.getStatusType(0));
     }
 }
